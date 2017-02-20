c870441148195125c33f1d01a6db0fa2cc4f1a31
Aleksander Drozd <aleksander.drozd@outlook.com>
2017-02-20 22:24:41 +0100

    Implemented exceptions

--- src/ShoppingCartExercise/NegativeCountException.java
+++ src/ShoppingCartExercise/NegativeCountException.java
@@ -1,4 +1,12 @@
 package ShoppingCartExercise;
 
 public class NegativeCountException extends Exception {
+
+    public NegativeCountException(String message){
+        super(message);
+    }
+
+    public NegativeCountException(String message, Throwable exception){
+        super(message, exception);
+    }
 }
--- src/ShoppingCartExercise/NoSuchItemException.java
+++ src/ShoppingCartExercise/NoSuchItemException.java
@@ -1,4 +1,12 @@
 package ShoppingCartExercise;
 
 public class NoSuchItemException extends Exception {
+
+    public NoSuchItemException(String message){
+        super(message);
+    }
+
+    public NoSuchItemException(String message, Throwable exception){
+        super(message, exception);
+    }
 }
