6fdb987a6c3e82eec23136e986621daa754d9944
Aleksander Drozd <aleksander.drozd@outlook.com>
2017-02-20 15:19:53 +0100

    Added first test

--- Tests/StackTest.java
+++ Tests/StackTest.java
@@ -1,14 +1,25 @@
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class StackTest {
 
+    private static StackExercise stack;
+    
     @BeforeClass
-    public void setUp(){
-        Stack stack = new Stack();
+    public static void setUp(){
+        stack = new Stack();
     }
 
-    
+    @Test
+    public void test1() throws Exception{
+        assertTrue(stack.isEmpty());
 
+        stack.push("Item");
+        assertFalse(stack.isEmpty());
+
+        stack.pop();
+        assertTrue(stack.isEmpty());
+    }
 }
