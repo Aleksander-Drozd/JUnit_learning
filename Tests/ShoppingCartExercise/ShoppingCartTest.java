7105805a3def11d20e33e743f60ca0eab066dd06
Aleksander Drozd <aleksander.drozd@outlook.com>
2017-02-20 22:40:33 +0100

    Added new tests for add - delete oeprations

--- Tests/ShoppingCartExercise/ShoppingCartTest.java
+++ Tests/ShoppingCartExercise/ShoppingCartTest.java
@@ -1,16 +1,25 @@
 package ShoppingCartExercise;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ShoppingCartTest {
 
-    ShoppingCart shoppingCart;
+    private ShoppingCart shoppingCart;
+    private static Item bread, egg, milk;
+
+    @BeforeClass
+    public static void setUp(){
+        bread = new Item("Bread", 2.99f);
+        egg = new Item("Egg", 2.99f);
+        milk = new Item("Milk", 2.99f);
+    }
 
     @Before
-    public void setUp(){
+    public void init(){
         shoppingCart = new ShoppingCart();
     }
 
@@ -18,15 +27,35 @@ public class ShoppingCartTest {
     public void addingItemsToEmptyCart() throws Exception{
         assertEquals(0, shoppingCart.itemCount());
 
-        shoppingCart.addItems(new Item("Bread", 2.99f), 2);
-        shoppingCart.addItems(new Item("Egg", 0.99f), 6);
-        shoppingCart.addItems(new Item("Milk", 1.99f), 1);
+        shoppingCart.addItems(bread, 2);
+        shoppingCart.addItems(egg, 6);
+        shoppingCart.addItems(milk, 1);
 
         assertEquals(9, shoppingCart.itemCount());
     }
 
     @Test(expected = NoSuchItemException.class)
     public void deleteItems_emptyCart_ExceptionThrown() throws Exception{
+        assertEquals(0, shoppingCart.itemCount());
         shoppingCart.deleteItems(new Item("Bread", 2.30f), 2);
+    }
+
+    @Test(expected = NoSuchItemException.class)
+    public void deletedItems_emptyCart_ExceptionThrown() throws Exception{
+        addAndThenDeleteItemWithItemCountCheck(bread, 2, 0 );
+        addAndThenDeleteItemWithItemCountCheck(egg, 2, 0 );
+        addAndThenDeleteItemWithItemCountCheck(milk, 2, 0 );
+
+    }
+
+    private void addAndThenDeleteItemWithItemCountCheck(Item item, int quantity, int currentItemCount) throws Exception{
+        assertEquals(0, shoppingCart.itemCount());
+        shoppingCart.addItems(item, quantity);
+        currentItemCount += quantity;
+        assertEquals(currentItemCount, shoppingCart.itemCount());
+
+        shoppingCart.deleteItems(item, quantity);
+        currentItemCount -= quantity;
+        assertEquals(currentItemCount, shoppingCart.itemCount());
     }
 }
